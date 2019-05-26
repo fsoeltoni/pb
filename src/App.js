206dfe09f7e831e9326510f82976bae3f4b61d88
fsoeltoni <fsoeltoni@gmail.com>
2019-05-27 02:23:38 +0700

    Create res pengguna

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import React from "react";
 import { Admin } from "react-admin";
 import attrs from "./consts/attrs";
 import data from "./providers/data";
+import { Resource } from "ra-core";
+import { PENGGUNA } from "./resources/pengguna/consts";
 
 function App() {
-  return <Admin title={attrs.title} dataProvider={data} />;
+  return (
+    <Admin title={attrs.title} dataProvider={data}>
+      <Resource name={PENGGUNA.res} options={{ label: PENGGUNA.lab }} />
+    </Admin>
+  );
 }
 
 export default App;
---
+++ src/resources/pengguna/consts.js
@@ -0,0 +1,18 @@
+export const PENGGUNA = {
+  res: "pengguna",
+  lab: "Pengguna",
+  field: {
+    id: {
+      res: "id",
+      lab: "Id"
+    },
+    nrp: {
+      res: "nrp",
+      lab: "NRP"
+    },
+    kata_sandi: {
+      res: "kata_sandi",
+      lab: "Kata Sandi"
+    }
+  }
+};
